cd449de75e25be26cbd59d756305c4b3cf371c6d
Julian <vanegas2001@gmail.com>
2023-09-18 18:09:02 -0500

    HA-5

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,21 +1,38 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 import '../Stylesheets/Calendar.css';
 import axios from 'axios';
 import esLocale from '@fullcalendar/core/locales/es';
+import Modal from 'react-bootstrap/Modal';
 import PropTypes from 'prop-types';
 
 function Calendar({ serviceData }) {
 	const [hollidays, setHollidays] = useState([]);
 	const [appointments, setAppointments] = useState([]);
+	const [modalShow, setModalShow] = React.useState(false);
+	const [dueDate, setDueDate] = useState(null);
+
+	const handleDateChangee = event => {
+		setDueDate(event.target.value);
+		event.preventDefault();
+	};
+
+	const handleDateClick = arg => {
+		const fecha = arg.date;
+		const dia = fecha.getDate().toString().padStart(2, '0');
+		const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+		const año = fecha.getFullYear();
+		const fechaFormateada = `${año}-${mes}-${dia}`;
+		setModalShow(true);
+		setDueDate(fechaFormateada);
+	};
 
 	useEffect(() => {
 		fetchData();
 		fetchData2();
-		console.log(serviceData);
 	}, []);
 
 	function fetchData() {
@@ -40,6 +57,7 @@ function Calendar({ serviceData }) {
 				console.error(error);
 			});
 	}
+
 	function fetchData2() {
 		const options = {
 			method: 'GET',
@@ -62,11 +80,76 @@ function Calendar({ serviceData }) {
 				console.error(error);
 			});
 	}
+	function ModalForm(props) {
+		return (
+			<Modal
+				{...props}
+				size='lg'
+				aria-labelledby='contained-modal-title-vcenter'
+				centered
+				className='modal'
+			>
+				<Modal.Header closeButton>
+					<Modal.Title id='contained-modal-title-vcenter'>
+						Solicitud de cita
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form className='form-modal'>
+						<Form.Group className='mb-3' controlId='formGrouService'>
+							<Form.Label>Servicio</Form.Label>
+							<p>
+								{serviceData.code} - {serviceData.name}
+							</p>
+						</Form.Group>
+						<Form.Group className='mb-3' controlId='formGroupDate'>
+							<Form.Label>Fecha</Form.Label>
+							<Form.Control
+								type='date'
+								placeholder='Due date'
+								value={dueDate}
+								onChange={handleDateChangee}
+							/>
+						</Form.Group>
+						<Form.Group className='mb-3' controlId='formGroupTime'>
+							<Form.Label>Hora</Form.Label>
+							<Form.Control type='time' placeholder='Hora' />
+						</Form.Group>
+						<Form.Group className='mb-3' controlId='formGroupPatient'>
+							<Form.Label>Paciente</Form.Label>
+							<Form.Select>
+								<option value='blanco'> </option>
+								{appointments.map((opcion, index) => (
+									<option key={index} value={opcion}>
+										{opcion.id}
+									</option>
+								))}
+							</Form.Select>
+						</Form.Group>
+						<Form.Group className='mb-3' controlId='formGroupReason'>
+							<Form.Label>Motivo de consulta</Form.Label>
+							<Form.Control as='textArea' placeholder='Motivo' />
+						</Form.Group>
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					{/* eslint-disable-next-line react/prop-types */}
+					<Button className='closeButton' onClick={props.onHide}>
+						Cerrar
+					</Button>
+					{/* eslint-disable-next-line react/prop-types */}
+					<Button className='registerButton' onClick={props.onHide}>
+						Registro
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		);
+	}
 
 	return (
 		<div>
 			<Container fluid>
-				<Row className='p-5 m-0'>
+				<Row className=' p-5 m-0'>
 					<Col className='container2'>
 						<Col className='container3'>
 							<Row className='custom-tittle'>
@@ -79,6 +162,7 @@ function Calendar({ serviceData }) {
 								locale={esLocale}
 								className='calendar'
 								plugins={[dayGridPlugin, interactionPlugin]}
+								dateClick={handleDateClick}
 								headerToolbar={{
 									left: 'prev',
 									center: 'title',
@@ -125,6 +209,7 @@ function Calendar({ serviceData }) {
 					</Col>
 				</Row>
 			</Container>
+			<ModalForm show={modalShow} onHide={() => setModalShow(false)} />
 		</div>
 	);
 }
